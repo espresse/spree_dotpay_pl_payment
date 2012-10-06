034a882be272cb46ddb199f4b68bf617d9f322fb
Dominik Fijas <dominik@applicake.com>
2012-10-06 21:33:06 +0200

    fixes paths and model names

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -6,9 +6,9 @@ Spree::CheckoutController.class_eval do
 
   def redirect_for_dotpay_pl
     return unless params[:state] == "payment"
-    @payment_method = PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
+    @payment_method = Spree::PaymentMethod.find(params[:order][:payments_attributes].first[:payment_method_id])
     if @payment_method && @payment_method.kind_of?(PaymentMethod::DotpayPl)
-      redirect_to gateway_dotpay_pl_path(:gateway_id => @payment_method.id, :order_id => @order.id)
+      redirect_to main_app.gateway_dotpay_pl_path(:gateway_id => @payment_method.id, :order_id => @order.id)
     end
   end
 
